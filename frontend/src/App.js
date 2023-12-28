1bf8eb757051ef64aea933b263698e14b4517737
anuHait <anusmitahait67@gmail.com>
2023-12-28 21:35:57 +0530

    new changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,16 @@ import Signin from "./modules/Signin";
 import Dashboard from "./modules/Dashboard";
 import { Routes, Route, Navigate } from "react-router-dom";
 
-const ProtectedRoute = ({ children }) => {
-  const isLoggedIn = localStorage.getItem('user:token') !== null;
+// const ProtectedRoute = ({ children }) => {
+//   const isLoggedIn = localStorage.getItem('user:token') !== null;
 
-  if (!isLoggedIn) {
-    // Redirect to the signin page if not logged in
-    return <Navigate to="/" replace />;
-  }
+//   if (!isLoggedIn) {
+//     // Redirect to the signin page if not logged in
+//     return <Navigate to="/" replace />;
+//   }
 
-  return children;
-};
+//   return children;
+// };
 
 function App() {
   return (
@@ -21,9 +21,9 @@ function App() {
         <Route
           path="/dashboard"
           element={
-            <ProtectedRoute>
+           
               <Dashboard />
-            </ProtectedRoute>
+            
           }
         />
       </Routes>
